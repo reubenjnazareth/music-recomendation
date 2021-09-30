8f51fe23c4644f0407f4e468c059b958b181ed3e
rnaz25 <63872943+rnaz25@users.noreply.github.com>
2021-09-30 11:26:12 +0530

    update alignment

--- src/App.js
+++ src/App.js
@@ -3,33 +3,33 @@ import "./styles.css";
 
 var songs = {
   Pop: [
-    { name: "Anyone", artist: "Justin Bieber" },
-    { name: "Drivers Liscense", artist: "Olivia Rodrigo" },
-    { name: "Watermelon Sugar", artist: "Harry Styles" },
-    { name: "At My Worst", artist: "Pink Sweat$" },
-    { name: "Dont Start Now", artist: "Dua Lipa" }
+    { name: "Anyone", artist: "Justin Bieber", Rating: "5/5" },
+    { name: "Drivers Liscense", artist: "Olivia Rodrigo", Rating: "5/5" },
+    { name: "Watermelon Sugar", artist: "Harry Styles", Rating: "4/5" },
+    { name: "At My Worst", artist: "Pink Sweat$", Rating: "3/5" },
+    { name: "Dont Start Now", artist: "Dua Lipa", Rating: "3/5" }
   ],
   Indie: [
-    { name: "I Wish", artist: "Faime" },
-    { name: "Slow Dance", artist: "Vernic" },
-    { name: "Why", artist: "Zodiac Wave" },
-    { name: "Run Away Fade Away", artist: "Ribhavv" },
-    { name: "Take Three", artist: "Pelle" }
+    { name: "I Wish", artist: "Faime", Rating: "4/5" },
+    { name: "Slow Dance", artist: "Vernic", Rating: "4/5" },
+    { name: "Why", artist: "Zodiac Wave", Rating: "3/5" },
+    { name: "Run Away Fade Away", artist: "Ribhavv", Rating: "3/5" },
+    { name: "Take Three", artist: "Pelle", Rating: "3/5" }
   ],
 
   Punjabi: [
-    { name: "Ma Belle", artist: "AP Dillion, Amari" },
-    { name: "Band Darvaza", artist: "Amrinder Gill" },
-    { name: "Lover", artist: "Diljit Dosanjh" },
-    { name: "Champagne", artist: "Diljit Dosanjh" },
-    { name: "Into You", artist: "Tegi Pannu" }
+    { name: "Ma Belle", artist: "AP Dillion, Amari", Rating: "5/5" },
+    { name: "Band Darvaza", artist: "Amrinder Gill", Rating: "4/5" },
+    { name: "Lover", artist: "Diljit Dosanjh", Rating: "3/5" },
+    { name: "Champagne", artist: "Diljit Dosanjh", Rating: "3/5" },
+    { name: "Into You", artist: "Tegi Pannu", Rating: "3/5" }
   ],
   Rock: [
-    { name: "Stairway to Heaven", artist: "Led Zepplin" },
-    { name: "Bohemian Raphsody", artist: "Queen" },
-    { name: "Smoke on the Water", artist: "Deep Purple" },
-    { name: "Comfortably Numb", artist: "Pink Floyd" },
-    { name: "Kashmir", artist: "Led Zepplin" }
+    { name: "Stairway to Heaven", artist: "Led Zepplin", Rating: "5/5" },
+    { name: "Bohemian Raphsody", artist: "Queen", Rating: "5/5" },
+    { name: "Smoke on the Water", artist: "Deep Purple", Rating: "5/5" },
+    { name: "Comfortably Numb", artist: "Pink Floyd", Rating: "5/5" },
+    { name: "Kashmir", artist: "Led Zepplin", Rating: "4/5" }
   ]
 };
 
@@ -43,7 +43,7 @@ export default function App() {
   }
 
   return (
-    <div className="App">
+    <div style={{ textAlign: "center" }} className="App">
       <h1>MUSIC</h1>
 
       <h2>Check out my music recommendations based on different genres!</h2>
@@ -70,9 +70,19 @@ export default function App() {
       <ul>
         {songs[genreWanted].map(function (result) {
           return (
-            <li key={result.name} style={{ padding: "0.5rem" }}>
+            <li
+              key={result.name}
+              style={{
+                border: "0.5px solid",
+                borderRadius: "0.6rem",
+                margin: "20px",
+                listStyle: "none",
+                padding: "0.5rem"
+              }}
+            >
               <div>{result.name} </div>
               <div style={{ fontSize: "small" }}>{result.artist} </div>
+              <div style={{ fontSize: "small" }}>{result.Rating} </div>
             </li>
           );
         })}
